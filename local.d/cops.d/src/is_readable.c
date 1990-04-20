30a3757d38a4a75c572b2b356cb21f33a683c3ee
vrs <vrs@93276802-5317-4dee-a4ef-7ae40ebda4b9>
1990-04-20 16:30:23 +0000

    Initial version
    git-svn-id: https://svn.so-much-stuff.com/svn/trunk/cvs/trunk@1575 93276802-5317-4dee-a4ef-7ae40ebda4b9

---
+++ local.d/cops.d/src/is_readable.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <math.h>
+#include <sys/stat.h>
+
+#define G_WRITE 0040	/* group readable */
+#define W_WRITE 0004	/* world readable */
+
+main(argc,argv)
+char **argv;
+{
+	register int group = 0, xmode = 0;
+	static struct stat statb;
+
+	if (argc < 2) {
+		printf("Usage: %s [-g] file\n",argv[0]);
+		exit(0);
+	}
+
+	if (argc > 2) {
+		if (!strcmp(argv[1], "-g")) {
+			group = 1;
+			argc--;
+			argv++;
+		}
+	}
+
+	if (stat(*++argv,&statb) < 0) {
+		exit(2);
+	}
+
+	if (group) xmode = statb.st_mode & G_WRITE;
+	else xmode = statb.st_mode & W_WRITE;
+
+	exit(!xmode);
+}
---
+++ local.d/cops.d/src/user.chk.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* Any file writable by all will be flagged */
+#define DMODE 002
+
+/* #define DMODE2 020 */
+
+/* potentially dangerous files */
+char *ftable[] = {
+	"rhost",
+	"profile",
+	"login",
+	"cshrc",
+	"bashrc",
+	"kshrc",
+	"tcshrc",
+	"rhost",
+	"netrc",
+	"forward",
+	"dbxinit",
+	"distfile",
+	"exrc",
+	"emacsrc"
+};
+char *ft;
+char *ftr, *malloc();
+
+char generic_file[100];
+
+main(argc,argv)
+int argc;
+char **argv;
+{
+register int fmode;
+/*
+int index;
+*/
+register int index;
+struct passwd *pp;
+static struct stat statb;
+
+if (argc != 1) {
+	printf("Usage: %s\n",argv[0]);
+	exit(1);
+}
+
+ft = malloc(100);
+ftr = malloc(100);
+
+while ((pp = getpwent()) != (struct passwd *)0) {
+	if (stat(pp->pw_dir,&statb) < 0) {
+		continue;
+	}
+
+	index = 0;
+	/*
+	 *   Use the home-dir, and add on each potential security threat
+	 * file to the path one at a time.  Then check each file to see
+	 * if it breaks with the modes established up above
+	 *
+	*/
+	for (ft = ftable[index]; index < 13; ft = ftable[++index]) {
+		if (strlen(pp->pw_dir) != 1)
+			sprintf(generic_file, "%s/.%s", pp->pw_dir,ft);
+		else 
+			sprintf(generic_file, "%s.%s", pp->pw_dir,ft);
+
+		if (stat(generic_file,&statb) < 0)
+			continue;
+
+		if (statb.st_mode & DMODE) 
+			printf("Warning!  User %s:\t%s is mode \t0%3.3o!\n",
+	       		pp->pw_name,generic_file,statb.st_mode&~S_IFMT);
+
+	}
+
+}
+
+exit(0);
+}
