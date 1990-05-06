54913060b110dde19f8cf43e3049339442722e82
vrs <vrs@93276802-5317-4dee-a4ef-7ae40ebda4b9>
1990-05-06 22:36:28 +0000

    Patch to work in SYS5DIR case
    git-svn-id: https://svn.so-much-stuff.com/svn/trunk/cvs/trunk@1602 93276802-5317-4dee-a4ef-7ae40ebda4b9

--- local.d/mh.d/h/local.h
+++ local.d/mh.d/h/local.h
@@ -7,11 +7,15 @@
 #endif	BSD42
 
 #ifndef	BSD42
+#ifndef	SYS5DIR
 #ifndef NDIR
 #include <dir.h>
 #else	NDIR
 #include <ndir.h>
 #endif  NDIR
+#else	SYS5DIR
+#include <dirent.h>
+#endif	SYS5DIR
 #else	BSD42
 #include <sys/dir.h>
 #endif	BSD42
