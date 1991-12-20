0730ab832c253d5374f2a1a78183c85d244e7d74
vrs <vrs@93276802-5317-4dee-a4ef-7ae40ebda4b9>
1991-12-20 03:42:26 +0000

    Add check of physical address before declaring a buffer missing
    git-svn-id: https://svn.so-much-stuff.com/svn/trunk/cvs/trunk@1716 93276802-5317-4dee-a4ef-7ae40ebda4b9

--- io.d/tools.d/cbuf.c
+++ io.d/tools.d/cbuf.c
@@ -7,54 +7,56 @@
 */
 #include <sys/types.h>
 #include <sys/param.h>
+#include <sys/sysmacros.h>
+#include <sys/immu.h>
 #include <sys/lock.h>
 #include <sys/tty.h>
 #include <stdio.h>
 #include <nlist.h>
 
 struct	nlist nl[] = {
+	{ "kpd0",		0L, 0, 0, 0, 0 },
+#define	X_KPD0	0					/* Kernel page directory	*/
 	{ "cfreelist",	0L, 0, 0, 0, 0 },
-#define	X_CFREELIST	0	/* Cbuf free list head		*/
+#define	X_CFREELIST	(X_KPD0+1)		/* Cbuf free list head		*/
 	{ "cfree",		0L, 0, 0, 0, 0 },
-#define	X_CFREE		1	/* Cbuf Starting address	*/
+#define	X_CFREE		(X_CFREELIST+1)	/* Cbuf Starting address	*/
 	{ "cfreecnt",	0L, 0, 0, 0, 0 },
-#define	X_CFREECNT	2	/* # of cbuf				*/
+#define	X_CFREECNT	(X_CFREE+1)		/* # of cbuf				*/
 	{ "N8251",		0L, 0, 0, 0, 0 },
-#define	X_N8251		3	/* # of 8251 port 			*/
-	{ "N546",		0L, 0, 0, 0, 0 },
-#define	X_N546		4	/* # of 546 board 			*/
-	{ "n_354",		0L, 0, 0, 0, 0 },
-#define	X_N354		5	/* # of 354 board 			*/
-	{ "MaxATCSLines",0L,0, 0, 0, 0 },
-#define	X_NATCS		6	/* # of atcs port  			*/
+#define	X_N8251		(X_CFREECNT+1)	/* # of 8251 port 			*/
 	{ "i8251_tty",	0L, 0, 0, 0, 0 },
-#define	X_I8251		7	/* i8251 tty 				*/
+#define	X_I8251		(X_N8251+1)		/* i8251 tty 				*/
+	{ "N546",		0L, 0, 0, 0, 0 },
+#define	X_N546		(X_I8251+1)		/* # of 546 boards			*/
 	{ "i546_tty",	0L, 0, 0, 0, 0 },
-#define	X_I546		8	/* i546 tty 				*/
+#define	X_I546		(X_N546+1)		/* i546 tty 				*/
+	{ "n_354",		0L, 0, 0, 0, 0 },
+#define	X_N354		(X_I546+1)		/* # of 354 board 			*/
 	{ "i354_tty",	0L, 0, 0, 0, 0 },
-#define	X_I354		9	/* i354 tty 				*/
+#define	X_I354		(X_N354+1)		/* i354 tty 				*/
+	{ "MaxATCSLines",0L,0, 0, 0, 0 },
+#define	X_NATCS		(X_I354+1)		/* # of atcs port			*/
 	{ "atcs_tty",	0L, 0, 0, 0, 0 },
-#define	X_IATCS		10	/* atcs tty 				*/
+#define	X_IATCS		(X_NATCS+1)		/* atcs tty 				*/
 	{ "Nspt",		0L, 0, 0, 0, 0 },
-#define	X_NSPT		11	/* atcs tty 				*/
+#define	X_NSPT		(X_IATCS+1)		/* atcs tty 				*/
 	{ "spt_tty",	0L, 0, 0, 0, 0 },
-#define	X_ISPT		12	/* atcs tty 				*/
+#define	X_ISPT		(X_NSPT+1)		/* atcs tty 				*/
 	{ "nkdvtty",	0L, 0, 0, 0, 0 },
-#define	X_NKD		13	/* # of kd 					*/
+#define	X_NKD		(X_ISPT+1)		/* # of kd 					*/
 	{ "kd_tty",		0L, 0, 0, 0, 0 },
-#define	X_IKD		14	/* kd tty 					*/
-	{ "serial_ports", 0L, 0, 0, 0, 0 },
-#define	X_NSERIAL	15	/* # of serial				*/
-	{ "serial_tty",	0L, 0, 0, 0, 0 },
-#define	X_ISERIAL	16	/* serial 					*/
+#define	X_IKD		(X_NKD+1)		/* kd tty 					*/
 	{ "asy_tty",	0L, 0, 0, 0, 0 },
-#define	X_IASY		17	/* asy tty					*/
-#ifdef later
+#define	X_IASY		(X_IKD+1)		/* asy tty					*/
+	{ "serial_ports", 0L, 0, 0, 0, 0 },
+#define	X_NSERIAL	(X_IASY+1)		/* # of serial				*/
+	{ "serial_tty",	0L, 0, 0, 0, 0 },
+#define	X_ISERIAL	(X_NSERIAL+1)	/* serial 					*/
 	{ "n_i3xxcp",	0L, 0, 0, 0, 0 },
-#define	X_N3XXCP	18	# of i3xxcp 				
+#define	X_N3XXCP	(X_ISERIAL+1)	/* # of i3xxcp 				*/
 	{ "i3xxcp_tty",	0L, 0, 0, 0, 0 },
-#define	X_I3XXCP	19	i3xxcp tty 				
-#endif /*later*/
+#define	X_I3XXCP	(X_N3XXCP+1)	/* i3xxcp tty 				*/
 	{ 0,			0L, 0, 0, 0, 0 }
 };
 
@@ -74,6 +76,30 @@ int	portkd, portserial, port8251, port546, port354, portatcs, portspt;
 
 int	kmem;				/* Kernel memory					*/
 long	lseek();
+
+pde_t *kpd0;			/* Copy of kernel page directory	*/
+
+paddr_t
+svirtophys(vaddr)
+unsigned long vaddr;
+{	paddr_t retval;
+	pde_t *kptbl;
+	pde_t kpte;
+
+	/*
+	 *	Find and read the correct page table entry.
+	*/
+	kptbl = vatopde(vaddr, (kpd0+ptnum(vaddr)));
+	(void) lseek(kmem, (long)kptbl, 0);
+	(void) read(kmem, (char *)&kpte, sizeof(kpte));
+	if (kpte.pgm.pg_pres == 0) {
+		fprintf(stderr, "svirtophys - not present.\n");
+		abort(1);
+	}
+	retval = ctob(kpte.pgm.pg_pfn) + poff(vaddr);
+	return((paddr_t) retval);
+}
+
 /*
  *	Take a snapshot of memory
  *	RETURN: pointer of memory that saved information
@@ -92,7 +118,7 @@ char *ptr;			/* Place for results			*/
 	if (read(kmem, ptr, size) == -1) {
 		(void) fprintf(stderr, "Cannot read %d bytes at 0x%x\n", size, addr);
 		perror("read");
-		(void) exit(1);
+		(void) abort(1);
 	}
 }
 
@@ -126,13 +152,22 @@ int port;				/* port #					*/
 struct cblock *ptr;		/* pointer to cblock		*/
 {
 	while (ptr != NULL) {
+		if (ptr-cfree > cfreecnt) {
+			printf("cblock cnt == %d\n", cfreecnt);
+			printf("Bogus cblock %s%s=0x%x, port %d, cblock %d\n", str1,
+				str2, ptr, port, ptr-cfree);
+			status++;
+			return(1);
+		}
 		if (++portused[ptr-cfree] > 1) {
+			printf("%s%s=0x%x, port %d, cblock %d = %d\n", str1,
+				str2, ptr, port, ptr-cfree, portused[ptr-cfree]);
 			status++;
 			return(1);
 		}
 		if (verbose) {
-			printf("%s%s=%x, port %d, cblock %d = %d\n", str1,
-				str2,ptr, port, ptr-cfree, portused [ptr-cfree]);
+			printf("%s%s=0x%x, port %d, cblock %d = %d\n", str1,
+				str2, ptr, port, ptr-cfree, portused[ptr-cfree]);
 		}
 		ptr = cpool[ptr-cfree].c_next;
 	}
@@ -145,7 +180,7 @@ main(argc, argv)
 int argc;
 char *argv[];
 {
-	struct cblock *head;
+	struct cblock *head, *p;
 	int cnt;
 
 	if ((argc > 1) && (argv[1][0] == '-')) {
@@ -162,23 +197,38 @@ char *argv[];
 		(void) fprintf(stderr, "No namelist for /unix\n");
 		(void) exit(1);
 	}
+	/*
+	 *	Read the kernel page directory.  We'll need it for address
+	 *	translation later.
+	*/
+	kpd0 = (pde_t *)malloc(NPGPT*sizeof(*kpd0));
+	if (kpd0 == NULL) {
+		(void) fprintf(stderr, "kpd0 malloc failed\n");
+		(void) exit(1);
+	}
+	(void) lseek(kmem, nl[X_KPD0].n_value, 0);
+	(void) read(kmem, (char *)kpd0, NPGPT*sizeof(*kpd0));
+	/*
+	 *	Find out about the cblock free list.
+	*/
 	(void) lseek(kmem, nl[X_CFREE].n_value, 0);
 	(void) read(kmem, (char *)&cfree, sizeof(cfree));
 	(void) lseek(kmem, nl[X_CFREECNT].n_value, 0);
 	(void) read(kmem, (char *)&cfreecnt, sizeof(int));
+	cfreecnt++;
 	/*
 	 *	Allocate and initialize storage for a copy of the cblock area.
 	*/
-	if ((cpool = (struct cblock *)malloc(sizeof(*cpool)*cfreecnt )) == NULL) {
+	if ((cpool = (struct cblock *)malloc(sizeof(*cpool)*cfreecnt)) == NULL) {
 		(void) fprintf(stderr, "Can not allocate cpool\n");
 		(void) exit(1);
 	}
-	if ((portused = (int *)malloc( sizeof(int)*cfreecnt )) == NULL) {
+	if ((portused = (int *)malloc(sizeof(int)*cfreecnt)) == NULL) {
 		(void) fprintf(stderr, "Can not allocate portused[]\n");
 		(void) exit(1);
 	}
-	for (cnt=0; cnt < cfreecnt; cnt++) {
-			portused[cnt] = 0;	/* Initialize 			*/
+	for (cnt = 0; cnt < cfreecnt; cnt++) {
+		portused[cnt] = 0;	/* Initialize 			*/
 	}
 
 	/*
@@ -206,12 +256,12 @@ char *argv[];
 		(void) read(kmem, (char *)&portatcs, sizeof(portatcs));
 		ttyatcs = (struct tty *)malloc(portatcs*sizeof(*ttyatcs));
 	}
-	if (nl[X_NSPT].n_sclass != 0)  {
+	if (nl[X_NSPT].n_sclass != 0) {
 		(void) lseek(kmem, nl[X_NSPT].n_value, 0);
 		(void) read(kmem, (char *)&portspt, sizeof(portspt));
 		ttyspt = (struct tty *)malloc(portspt*sizeof(*ttyspt));
 	}
-	if (nl[X_NKD].n_sclass != 0)  {
+	if (nl[X_NKD].n_sclass != 0) {
 		(void) lseek(kmem, nl[X_NKD].n_value, 0);
 		(void) read(kmem, (char *)&portkd, sizeof(portkd));
 		ttykd = (struct tty *)malloc(portkd*sizeof(*ttykd));
@@ -219,7 +269,7 @@ char *argv[];
 	if (nl[X_IASY].n_sclass != 0) {
 		ttyasy = (struct tty *)malloc(2*sizeof(*ttyasy));
 	}
-	if (nl[X_NSERIAL].n_sclass != 0)  {
+	if (nl[X_NSERIAL].n_sclass != 0) {
 		(void) lseek(kmem, nl[X_NSERIAL].n_value, 0);
 		(void) read(kmem, (char *)&portserial, sizeof(portserial));
 		ttyserial = (struct tty *)malloc(portserial*sizeof(*ttyserial));
@@ -233,7 +283,8 @@ char *argv[];
 	snapshot(nl[X_CFREELIST].n_value, sizeof(cfreehead), (char *)&cfreehead);
 	snapshot(cfree, sizeof(*cpool)*cfreecnt, (char *)cpool);
 	snapshot(nl[X_IKD].n_value, sizeof(*ttykd)*portkd, (char *)ttykd);
-	snapshot(nl[X_IASY].n_value, sizeof(*ttyasy)*portasy, (char *)ttyasy);
+	if (nl[X_IASY].n_sclass != 0)
+		snapshot(nl[X_IASY].n_value, sizeof(*ttyasy)*portasy, (char *)ttyasy);
 	snapshot(nl[X_ISERIAL].n_value, sizeof(*ttyserial)*portserial, (char *)ttyserial);
 	snapshot(nl[X_I8251].n_value, sizeof(*tty8251)*port8251, (char *)tty8251);
 	snapshot(nl[X_I546].n_value, sizeof(*tty546)*port546, (char *)tty546);
@@ -246,7 +297,8 @@ char *argv[];
 	snapshot(nl[X_CFREELIST].n_value, sizeof(cfreehead), (char *)&cfreehead);
 	snapshot(cfree, sizeof(*cpool)*cfreecnt, (char *)cpool);
 	snapshot(nl[X_IKD].n_value, sizeof(*ttykd)*portkd, (char *)ttykd);
-	snapshot(nl[X_IASY].n_value, sizeof(*ttyasy)*portasy, (char *)ttyasy);
+	if (nl[X_IASY].n_sclass != 0)
+		snapshot(nl[X_IASY].n_value, sizeof(*ttyasy)*portasy, (char *)ttyasy);
 	snapshot(nl[X_ISERIAL].n_value, sizeof(*ttyserial)*portserial, (char *)ttyserial);
 	snapshot(nl[X_I8251].n_value, sizeof(*tty8251)*port8251, (char *)tty8251);
 	snapshot(nl[X_I546].n_value, sizeof(*tty546)*port546, (char *)tty546);
@@ -254,9 +306,8 @@ char *argv[];
 	snapshot(nl[X_IATCS].n_value, sizeof(*ttyatcs)*portatcs, (char *)ttyatcs);
 	snapshot(nl[X_ISPT].n_value, sizeof(*ttyspt)*portspt, (char *)ttyspt);
 	/*
-	 *  END OF SNAPSHOT
-	 */
-
+	 *	END OF SNAPSHOT
+	*/
 	head = cfreehead.c_next;
 	if (dochain("Freelist","",0,head)) {
 		printf("The cblock free list has a loop in it\n", cnt);
@@ -269,11 +320,15 @@ char *argv[];
 	doports("354", tty354, port354);
 	doports("ATCS", ttyatcs, portatcs);
 	doports("VT", ttyspt, portspt);
-	for (cnt = 0; cnt < cfreecnt; cnt++)
+	p = (struct cblock *)kvtophys(cfree);
+	for (cnt = 0; cnt < cfreecnt; cnt++, p++) {
 		switch (portused[cnt]) {
 			case 0:
+				if (((long)p) % 65536 > 65536-sizeof(*p))
+					break;
 				printf("Cbuf %d has been lost\n", cnt);
 				status++;
+				break;
 			case 1:
 				break;
 			default:
@@ -281,5 +336,6 @@ char *argv[];
 						cnt, portused[cnt]);
 				status++;
 		}
+	}
 	exit(status);
 }
