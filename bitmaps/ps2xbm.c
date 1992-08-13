f3dd8ef32dc94af4c99d3a5372b12c93325e0961
vrs <vrs@93276802-5317-4dee-a4ef-7ae40ebda4b9>
1992-08-13 23:38:31 +0000

    Make portable wrt byte ordering
    git-svn-id: https://svn.so-much-stuff.com/svn/trunk/cvs/trunk@1737 93276802-5317-4dee-a4ef-7ae40ebda4b9

--- bitmaps/ps2xbm.c
+++ bitmaps/ps2xbm.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+int swab = 0;
+
+void
+output(byte)
+int byte;
+{	static int count = 0;
+	static unsigned bytes = 0x0;
+
+	if (!swab) {
+		printf("0x%02x, ", byte);
+		return;
+	}
+	count++;
+	bytes = (bytes<<8)|byte;
+	if (count++ == 3) {
+		printf("0x%02x, ", 0xFF&(bytes>>24));
+		printf("0x%02x, ", 0xFF&(bytes>>16));
+		printf("0x%02x, ", 0xFF&(bytes>> 8));
+		printf("0x%02x, ", 0xFF&(bytes));
+		count = 0;
+		bytes = 0x0;
+	}
+}
+
 main()
 {	int inimage = 0;
 	int height = 0;
@@ -15,6 +39,10 @@ main()
 			width = atoi(buf+7);
 		if (strncmp(buf, "\rtheimage", 8) == 0)
 			break;
+		if (sscanf(buf, " %d %d %*d bitdump", &width, &height) == 2) {
+			swab = 1;
+			break;
+		}
 	}
 	printf("#define bm_width %d\n", width);
 	printf("#define bm_height %d\n", height);
@@ -37,9 +65,12 @@ main()
 		}
 		byte <<= 4;
 		byte += (c&0x0F) + (isalpha(c)*9);	/* Assumes ASCII */
-		printf("0x%02x, ", byte);
+		output(byte);
 		if ((++count % 12) == 0) {
 			printf("\n");
+		}
+		if (!(count*8 % width)) {
+			output(0xFF);
 		}
 		if (count*8 == width*height)
 			break;
