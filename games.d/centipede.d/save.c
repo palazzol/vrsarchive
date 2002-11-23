8d5ddb09c7dd7e72fb526221fe0a9f02e8ddc0fa
Vincent <Vincent@93276802-5317-4dee-a4ef-7ae40ebda4b9>
2002-11-23 05:50:12 +0000

    Add termios support, use putwin (rather than implementation dependent curses.h data structures) during saves.
    git-svn-id: https://svn.so-much-stuff.com/svn/trunk/cvs/trunk@1774 93276802-5317-4dee-a4ef-7ae40ebda4b9

--- games.d/centipede.d/save.c
+++ games.d/centipede.d/save.c
@@ -10,7 +10,11 @@ extern struct passwd *getpwnam();
 savegame()
 {
     char fil[100];
+#ifdef __STDC__
+    struct termios curseterm;
+#else
     struct sgttyb curseterm;
+#endif
 
     strcpy(fil, "cent.save");
     if (!gamestarted)
@@ -30,10 +34,19 @@ savegame()
     echo();
     nl();
     printf("File name: ");
+#ifdef __STDC__
+    tcgetattr(0,&curseterm);
+    tcsetattr(0,TCSADRAIN,&origterm);
+#else
     ioctl(0,TIOCGETP,&curseterm);
     ioctl(0,TIOCSETP,&origterm);
+#endif
     scanf("%s",fil);
+#ifdef __STDC__
+    tcsetattr(0,TCSADRAIN,&curseterm);
+#else
     ioctl(0,TIOCSETP,&curseterm);
+#endif
     noecho();
     crmode();
     printf("Saving... ");
@@ -58,21 +71,46 @@ char *fil;
     int fd,y,x;
     char ch;
     PEDE *piece;
+#ifdef __STDC__
+    FILE *fp;
+#endif
     
     if ((fd = creat(fil,0600)) == -1)
     {
 	perror("Error opening file");
 	return(BAD);
     }
+#ifdef __STDC__
+    if ((fp = fdopen(fd,"wb")) == NULL)
+    {
+	perror("Error opening file");
+	close(fd);
+	return(BAD);
+    }
+#endif
     ch = MAGICNUM;
+#ifdef __STDC__
+
+    fwrite(&ch,1,1,fp);
+    fwrite(&version,sizeof(float),1,fp);
+    putwin(stdscr,fp);
+    fwrite(mushw,24 * 57,1,fp);
+#define wint(i)  fwrite(&i,sizeof(int),1,fp)
+#define wlong(i) fwrite(&i,sizeof(long),1,fp)
+#define wc(cd)   fwrite(&cd,sizeof(COORD),1,fp)
+
+#else
+
     write(fd,&ch,1);
     write(fd,&version,sizeof(float));
     for (y = 0; y < 24; y++)
 	write(fd,(stdscr->_y)[y],80);
     write(fd,mushw,24 * 57);
-
 #define wint(i) write(fd,&i,sizeof(int))
 #define wlong(i) write(fd,&i,sizeof(long))
+#define wc(cd) write(fd,&cd,sizeof(COORD))
+
+#endif
 
     wint(numpedes);
     wint(fired);
@@ -95,8 +133,6 @@ char *fil;
     wint(spiderdir);
     wint(spidcount);
 
-#define wc(cd) write(fd,&cd,sizeof(COORD))
-
     wc(guy);
     wc(shot);
     wc(flea);
@@ -108,6 +144,9 @@ char *fil;
 	write(fd,piece,sizeof(PEDE));
     tim = time(0);
     wlong(tim);
+#ifdef __STDC__
+    fclose(fp);
+#endif
     close(fd);
     chmod(fil,0);
     return(GOOD);
@@ -125,6 +164,9 @@ char *fil;
     PEDE **piece = &centipede, *prev = NULL;
     struct stat sbuf;
     float vers;
+#ifdef __STDC__
+    FILE *fp;
+#endif
 
     printf("Reading file... ");
     fflush(stdout);
@@ -136,26 +178,45 @@ char *fil;
 	BADF("That's not your game!\n");
     if (sbuf.st_nlink > 1)
 	BADF("Cannot restore from linked file.\n");
-    lseek(fd,-4,2);
+#ifdef __STDC__
+
+    if ((fp = fdopen(fd,"rb")) == NULL)
+	BADF("Error fdopening file");
+#define rint(i) fread(&i,sizeof(int),1,fp);
+#define rlong(i) fread(&i,sizeof(long),1,fp);
+    fseek(fp,-sizeof(tim),SEEK_END);
+    rlong(tim);
+    fseek(fp,0L,SEEK_SET);
+    fread(&ch,1,1,fp);
+
+#else
 
 #define rint(i) read(fd,&i,sizeof(int));
 #define rlong(i) read(fd,&i,sizeof(long));
-
+    lseek(fd,-4,2);
     rlong(tim);
     lseek(fd,0,0);
     read(fd,&ch,1);
+
+#endif
     if (sbuf.st_mtime > tim + 2 || sbuf.st_ctime > tim + 2 ||
       ch != MAGICNUM)
 	BADF("Sorry, but the file has been touched.\n");
+#ifdef __STDC__
+    fread(&vers,sizeof(float),1,fp);
+#else
     read(fd,&vers,sizeof(float));
+#endif
     if (version != vers)
 	BADF("Saved game is for the wrong version.\n");
+#ifdef __STDC__
+    stdscr = getwin(fp);
+    fread(mushw,24 * 57,1,fp);
+#else
     for (y = 0; y < 24; y++)
 	read(fd,(stdscr->_y)[y],80);
     read(fd,mushw,24 * 57);
-
-#define rint(i) read(fd,&i,sizeof(int));
-#define rlong(i) read(fd,&i,sizeof(long));
+#endif
 
     rint(numpedes);
     rint(fired);
@@ -197,6 +258,9 @@ char *fil;
     }
     lastpede = prev;
     *piece = NULL;
+#ifdef __STDC__
+    fclose(fp);
+#endif
     close(fd);
     unlink(fil);
     printf("Done\n");
