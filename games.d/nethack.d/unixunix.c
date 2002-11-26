3908383c0541921b65ceb9a867bfecff8658508b
vrs <vrs@93276802-5317-4dee-a4ef-7ae40ebda4b9>
2002-11-26 01:02:40 +0000

    Fix a problem that led to get_date being unresolved.
    git-svn-id: https://svn.so-much-stuff.com/svn/trunk/cvs/trunk@1968 93276802-5317-4dee-a4ef-7ae40ebda4b9

--- games.d/nethack.d/unixunix.c
+++ games.d/nethack.d/unixunix.c
@@ -48,7 +48,6 @@ getyear()
 	return(1900 + getlt()->tm_year);
 }
 
-#ifndef __STDC__
 char *
 get_date()
 {
@@ -61,7 +60,6 @@ get_date()
 	if(datestr[4] == ' ') datestr[4] = '0';
 	return(datestr);
 }
-#endif
 
 phase_of_the_moon()			/* 0-7, with 0: new, 4: full */
 {					/* moon period: 29.5306 days */
