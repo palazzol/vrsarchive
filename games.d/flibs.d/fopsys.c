b998a137988c8e626e50161658062c471b238687
Vince <Vince@93276802-5317-4dee-a4ef-7ae40ebda4b9>
2010-07-08 16:08:07 +0000

    Fix a portability issue with the use of rand().
    git-svn-id: https://svn.so-much-stuff.com/svn/trunk/cvs/trunk@2043 93276802-5317-4dee-a4ef-7ae40ebda4b9

--- games.d/flibs.d/fopsys.c
+++ games.d/flibs.d/fopsys.c
@@ -6,6 +6,9 @@
  * don't work out as well.
  */
 extern long time();
+#ifdef __STDC__
+#include <limits.h>
+#endif
 
 Oinit()			/* initialize */
 {
@@ -21,7 +24,11 @@ int lower,upper;		/* lower and upper (inclusive)		*/
 { extern int rand();
   float R;
   R = (float)rand();
+#ifdef __STDC__
+  R /= INT_MAX;
+#else
   R /= 32767;
+#endif
   R *= (upper - lower);
   R += lower;
   return( (int)(R + 0.5) );
